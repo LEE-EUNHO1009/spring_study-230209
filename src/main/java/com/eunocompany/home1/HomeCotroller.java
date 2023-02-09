ae186064218fc5e46379beba2d90b54995abd772
ici <ici@DESKTOP-G48V45S>
2023-02-09 12:57:46 +0900

    회원 포인트 추가

--- src/main/java/com/eunocompany/home1/HomeCotroller.java
+++ src/main/java/com/eunocompany/home1/HomeCotroller.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,7 +52,7 @@ public class HomeCotroller {
 		
 		return "joinOk";
 	}
-//==============================
+//==============================회원 포인트 창
 	@RequestMapping(value = "/point/{memberPoint}")
 	public String point(@PathVariable String memberPoint, Model model) {
 		
